d8c63549b2d86f26fca77fa7adb8045f7659bb39
Suyash Kamath <suyashkamath9@gmail.com>
2026-03-28 12:03:15 +0530

    Simple Values

---
+++ Lecture-5-Simple-Values/code/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Integers
+    fmt.Println(1)
+    fmt.Println(2)
+
+    // Strings
+    fmt.Println("Hello Golang")
+
+    // Booleans
+    fmt.Println(true)
+    fmt.Println(false)
+
+    // Floats
+    fmt.Println(10.5)
+    fmt.Println(7.0 / 3.0)
+}
