e92dc0b3a70dfc5ee9e1bf49ece02b74d277f321
Suyash Kamath <110880307+Suyash-Kamath@users.noreply.github.com>
2026-06-03 12:04:55 +0530

    Update goroutines.go

--- Lecture-24-Goroutines/code/goroutines.go
+++ Lecture-24-Goroutines/code/goroutines.go
@@ -1,5 +1,6 @@
 // Online Go compiler to run Golang program online
 // Print "Start small. Ship something." message
+// https://chatgpt.com/share/6a1fcae2-53c8-83e8-8b6d-f2becfd642e7
 
 package main
 import "fmt"
