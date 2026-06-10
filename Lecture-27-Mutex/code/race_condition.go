c3160efd5a71fe2a9460031079bd23e6efd56be4
Suyash Kamath <110880307+Suyash-Kamath@users.noreply.github.com>
2026-06-10 09:59:54 +0530

    Create race_condition.go

---
+++ Lecture-27-Mutex/code/race_condition.go
@@ -0,0 +1,37 @@
+package main
+
+
+import "fmt"
+import "sync"
+
+type post struct{
+    views int 
+    
+}
+
+func (p *post) inc(wg *sync.WaitGroup){
+    defer wg.Done()
+    p.views+=1
+}
+
+func main(){
+    
+    var wg sync.WaitGroup
+    
+    myPost:=post{
+        views:0,
+    }
+    
+    for i:=0;i<100;i++{
+        
+        wg.Add(1)
+         go myPost.inc(&wg)
+         
+    }
+   
+   wg.Wait()
+    
+    fmt.Println(myPost.views)
+    
+    
+}
