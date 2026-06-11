37429a0dd808437a30f564bb4967082fda6d56a1
Suyash Kamath <110880307+Suyash-Kamath@users.noreply.github.com>
2026-06-11 10:19:09 +0530

    Create user.go

---
+++ Lecture-29-Packages/code/user/user.go
@@ -0,0 +1,10 @@
+package user
+
+type User struct {
+	Name string
+	// User struct is captialized, so it is exported and can be used outside the package
+
+	// Name and Email is capitalized, so they are exported and can be used outside the package
+	Email string 
+}
+
