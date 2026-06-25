32332df0e6656db5d8eb31bf7d497af74afccad5
Suyash Kamath <110880307+Suyash-Kamath@users.noreply.github.com>
2026-06-25 15:28:40 +0530

    Add student creation handler
    Implement student creation handler with validation and error handling.

---
+++ REST-API/students-api/internal/http/handlers/student/student.go
@@ -0,0 +1,59 @@
+package student
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/Suyash-Kamath/Golang-Coders-Gyan/REST-API/students-api/internal/types"
+	"github.com/Suyash-Kamath/Golang-Coders-Gyan/REST-API/students-api/internal/utils/response"
+	"github.com/go-playground/validator/v10"
+)
+
+// crud
+
+// Create/ New is convention and this func returns handler func
+func New() http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		slog.Info("Creating a Student")
+
+		var student types.Student
+
+		// json ka decoder io.Reader naam ka interface chata hai and ye r *http.Request me interface implemented hota hai 
+		err:=json.NewDecoder(r.Body).Decode(&student)
+		// could have used nil but wanted to know explicitly ki kya wo wo error hai jo ham soch rahe hai , so error package hai , isme err pass karte hai , and type mention , io.EOF , means no more input is available (means body empty ,decode karne keliye kuch hai hi nahi )
+		if errors.Is(err,io.EOF){
+			// response.WriteJson(w,http.StatusBadRequest,response.GeneralError(err)) // returns error , but hum handle nahi karoge 
+			// Custom message 
+			response.WriteJson(w,http.StatusBadRequest,response.GeneralError(fmt.Errorf("Empty Body")))
+			return
+		}
+
+		if err!=nil{
+			response.WriteJson(w,http.StatusBadRequest,response.GeneralError(err))
+			return
+		}
+
+
+		// Request ko validate karna hi karna hai , 0 trust policy , har ek ko test karna hai kya send kiya wo kiya jo chahiye tha woh mila ?
+
+		// Request Validation
+
+		// could have done manually , but not recommened , we have package , validator
+
+		// struct tags daalo `validate:"required"` naam se 
+
+		if err:=validator.New().Struct(student);err!=nil{
+			// yahape alag type hai ValidationError , so typecast
+			validateErrs:= err.(validator.ValidationErrors)
+			response.WriteJson(w,http.StatusBadRequest,response.ValidationError(validateErrs))
+			return
+		}
+
+		response.WriteJson(w, http.StatusCreated,map[string]string{"success":"OK"})
+	}
+}
